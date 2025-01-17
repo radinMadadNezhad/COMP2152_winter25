e0d1028529f091fe9aa6b7fb2ca995fe982556ba
Radin madad nezhad aligorkeh <rad1383in@gmail.com>
2025-01-17 15:35:08 -0500

    completed the lab2

--- week02/lab02.py
+++ week02/lab02.py
@@ -1,5 +1,36 @@
+import random
+
 #Define the choices array
-choices = []
+choices = ["Rock", "paper", "Scissors"]
 
 def main():
-    return True
+    try:
+        user_input = input("Please enter your choice (Rock, paper, scissors): ").capitalize()
+
+        #Validate the user input
+        if user_input not in choices:
+            raise ValueError("Invalid choice!, Please enter Rock, Paper, Scissors")
+
+        # Convert the user input to an index
+        playerChoice = choices.index(user_input)
+
+        computerChoice = random.randint(0, 2)
+
+        print(f"Player choice:{choices[playerChoice]}")
+        print(f"Computer choice choice:{choices[computerChoice]}")
+
+        #Determine the winner
+        if playerChoice == computerChoice:
+            print("It's a tie!")
+        elif (playerChoice == 0 and computerChoice == 2) or \
+                (playerChoice == 1 and computerChoice == 0) or \
+                (playerChoice == 2 and computerChoice == 1):
+            print("player wins!")
+        else:
+            print("computer wins!")
+    except:
+
+
+# Run the game through the main function
+if __name__ == '__main__':
+    main()
